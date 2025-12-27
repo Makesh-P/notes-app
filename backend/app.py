b25693384bd669598295f50302bdb86c4de81e63
Makesh <makesh2876@gmail.com>
2025-12-27 20:23:03 +0530

    Fix create new to-do logic

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,8 @@ import json
 from datetime import datetime
 import os
 
-BASE_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
-
 
 app = Flask(__name__)
 CORS(app)
